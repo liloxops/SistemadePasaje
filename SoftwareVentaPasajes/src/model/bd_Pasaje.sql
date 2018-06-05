ebb54bd95365dcf2f1259d8227794f5ffa07daf2
Ángel Huerta <ahuerta@svr.lab315>
2018-06-05 06:31:59 -0400

    Se crea Base de Datos

---
+++ SoftwareVentaPasajes/src/model/bd_Pasaje.sql
@@ -0,0 +1,56 @@
+--Drop database BD_pasaje;
+create database BD_pasaje;
+
+use BD_pasaje;
+
+create table ciudad  (
+    id int auto_increment,
+    nombre varchar(30),
+    primary key(id)
+);
+create table conductor (
+    id int auto_increment,
+    nombre varchar(30),
+    primary key(id)
+);
+create table vendedor (
+    id int auto_increment,
+    nombre varchar(30),
+    rut varchar(12),
+    pass varchar(8),
+    primary key(id)
+);
+
+create table bus(
+    id int auto_increment,
+    placa varchar(10),
+    asiento int,
+    fk_conductor int,
+    foreign key (fk_conductor) references conductor(id),
+    primary key(id)
+);
+
+create table horarioBus (
+    id int auto_increment,
+    horario datetime,
+    fk_bus int,
+    foreign key (fk_bus) references bus(id),
+    primary key(id)
+);
+create table pasaje(
+    id int auto_increment,
+    fk_origen int,
+    fk_destino int,
+    fecha date,
+    fk_bus int,
+    fk_vendedor int,
+    foreign key (fk_origen) references ciudad(id),
+    foreign key (fk_destino) references ciudad(id),
+    foreign key (fk_bus) references bus(id),
+    foreign key (fk_vendedor) references vendedor(id),
+    primary key(id)
+);
+
+
+select * from pasaje;
+select * from horarioBus;
